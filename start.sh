5df3d929f4606ed19cdd8391e67f63392d730cd2
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-16 17:30:18 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ZauteKm/Message-Search-Bot /Message-Search-Bot
+cd /Message-Search-Bot
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
